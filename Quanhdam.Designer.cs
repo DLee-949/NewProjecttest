51467f637098b131ec1079ba6a2cc0c9e6cc80b1
Quang Anh <quanhdam308@gmail.com>
2025-09-09 18:06:35 +0700

    WAHOO

--- Quanhdam.Designer.cs
+++ Quanhdam.Designer.cs
@@ -29,6 +29,20 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lable12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label190257 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -36,18 +50,164 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(281, 129);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "AAAAAAAAAAAAA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giai Phuong trinh bac 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhap so lieu dau vao";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(38, 129);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(52, 31);
+            this.label20.TabIndex = 3;
+            this.label20.Text = "a =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 31);
+            this.label5.TabIndex = 4;
+            // 
+            // lable12
+            // 
+            this.lable12.AutoSize = true;
+            this.lable12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable12.Location = new System.Drawing.Point(449, 129);
+            this.lable12.Name = "lable12";
+            this.lable12.Size = new System.Drawing.Size(51, 31);
+            this.lable12.TabIndex = 5;
+            this.lable12.Text = "c =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(299, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 31);
+            this.label8.TabIndex = 8;
+            // 
+            // label190257
+            // 
+            this.label190257.AutoSize = true;
+            this.label190257.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label190257.Location = new System.Drawing.Point(241, 129);
+            this.label190257.Name = "label190257";
+            this.label190257.Size = new System.Drawing.Size(52, 31);
+            this.label190257.TabIndex = 7;
+            this.label190257.Text = "b =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 31);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Ket qua";
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKQ.Location = new System.Drawing.Point(60, 263);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(108, 31);
+            this.lblKQ.TabIndex = 10;
+            this.lblKQ.Text = "label 10";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(63, 464);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(332, 45);
+            this.btnOk.TabIndex = 11;
+            this.btnOk.Text = "Giai phuong trinh";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnd.Location = new System.Drawing.Point(856, 464);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(125, 45);
+            this.btnEnd.TabIndex = 12;
+            this.btnEnd.Text = "Ket thuc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(103, 137);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 22);
+            this.txtA.TabIndex = 13;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(318, 137);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 22);
+            this.txtB.TabIndex = 14;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(517, 139);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 22);
+            this.txtC.TabIndex = 15;
             // 
             // Quanhdam
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1041, 554);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label190257);
+            this.Controls.Add(this.lable12);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Quanhdam";
             this.Text = "Quanhdam";
+            this.Load += new System.EventHandler(this.Quanhdam_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +216,19 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lable12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label190257;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblKQ;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
     }
 }
--- Quanhdam.cs
+++ Quanhdam.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,71 @@ namespace NewProjecttest
 {
     public partial class Quanhdam : Form
     {
+        double a, b, c, d, x1, x2;
         public Quanhdam()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quanhdam_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                {
+                    a = Convert.ToDouble(txtA.Text);
+                    b = Convert.ToDouble(txtB.Text);
+                    c = Convert.ToDouble(txtC.Text);
+                    d = b * b - 4 * a * c;
+                }
+                if (d < 0)
+                {
+                    lblKQ.Text = "Phương trình vô nghiệm";
+                }
+                else if (d == 0)
+                {
+                    x1 = x2 = -b / (2 * a);
+                    lblKQ.Text = "Phương trình có nghiệm kép: x1 = x2 = " + Math.Round(x1, 1);
+                }
+                else
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    lblKQ.Text = "Phương trình có hai nghiệm phân biệt: x1 = " + Math.Round(x1, 1) + ", x2 = " + Math.Round(x2, 1);
+                }
+
+            }
+            catch (Exception)
+            {
+                lblKQ.Text= "Vui lòng nhập đúng định dạng số!";
+            }
+
+
+        }
     }
 }
+
